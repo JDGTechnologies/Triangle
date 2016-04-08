976eb9d764305b8b51ae7d5dea2b15cb01035865
JDGTechnologies <jdgtechn@gmail.com>
2016-04-08 10:19:59 -0500

    Triangle Class
    Input side lengths and will tell if the lengths are valid and what kind
    of triangle it is (scalene, isosceles or equilateral).

---
+++ src/triangle/Triangle.java
@@ -0,0 +1,63 @@
+package triangle;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Triangle {
+	int large=0;
+	int medium=0;
+	int small=0;
+	
+	public void setOrder(int a, int b, int c){
+		int[] s = {a,b,c};
+		
+		Arrays.sort(s);
+		
+		large = s[2];
+		medium =s[1];
+		small = s[0];
+	}
+	
+	public boolean valid(){
+		int tot = medium + small;
+		if(tot < large){
+			return false;
+		}
+		return true;
+	}
+	
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle something = new Triangle();
+		
+		System.out.println("Welcome to the Triangle Calculator");
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please Input Side 1:");
+		int a = reader.nextInt();
+		System.out.println("Please Input Side 2");
+		int b = reader.nextInt();
+		System.out.println("Please Input Side 3");
+		int c = reader.nextInt();
+		
+		something.setOrder(a,b,c);
+		
+		boolean v = something.valid();
+		
+		if(v == true){
+			if(a == b && a == c){
+				System.out.println("Equilateral");
+			}
+			else if (a != b && a != c && b != c){
+				System.out.println("Scalene");
+			}
+			else{
+				System.out.println("Isoceles");
+			}
+		}
+		else{
+			System.out.println("input is not valid");
+		}
+	}
+}
